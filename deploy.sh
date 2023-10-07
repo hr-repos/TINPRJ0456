153f7be2edd75f69730e15982c53bdde20b21198
Eli <JustEli@users.noreply.github.com>
2023-10-07 11:20:23 +0200

    docker compose voor het hele project

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd back-end
+
+echo -e "\n\n\nBuilding Java API back-end..."
+mvn clean package
+
+if [ $? -ne 0 ]; then
+    echo -e "\n\nMaven is not globally installed.\nBuild the back-end yourself with Maven.\n"
+    read -p "Did you do this? (y/n): " choice
+
+    if [ "$choice" != "y" ]; then
+        echo "Can't build Docker images. Exiting..."
+        sleep 5
+        exit 1
+    fi
+fi
+
+echo -e "\n\n\nBuilding Docker image for API..."
+docker build -t scr-api .
+
+cd ../front-end
+
+echo -e "\n\n\nBuilding Vue front-end dashboard..."
+npm install
+npm run build-only
+
+echo -e "\n\n\nBuilding Docker image for proxy, including dashboard..."
+docker build -t scr-proxy .
+
+cd ..
+
+
+echo -e "\n\n\nStarting up Docker compose containers..."
+docker-compose -p scr up -d
+
+
+echo "Build process completed."
