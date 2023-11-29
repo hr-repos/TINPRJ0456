b390e5dbf248726201462f2ab1719562b3dfa481
TigoG <tigogoes11@gmail.com>
2023-11-29 12:08:43 +0100

    Made a terminalComponent

---
+++ front-end/src/components/TerminalComponent.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+      <div class="terminal">
+        <p v-for="(message, index) in terminalMessages" :key="index">{{ message }}</p>
+      </div>
+      <div class="input-container">
+        <input v-model="inputString" @keyup.enter="sendToTerminal" placeholder="Type your command..." />
+        <button @click="sendToTerminal">Send</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        inputString: '',
+        terminalMessages: [],
+      };
+    },
+    methods: {
+      sendToTerminal() {
+        // Add the input string to the terminal messages
+        this.terminalMessages.push(`New project: ${this.inputString}`);
+  
+        // Handle the command (you can customize this logic based on your use case)
+        this.handleCommand(this.inputString);
+  
+        // Clear the input after sending
+        this.inputString = '';
+      },
+      handleCommand(command) {
+       // Handle the command here
+        this.terminalMessages.push(`Terminal: ${command}`);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .terminal {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin-bottom: 10px;
+    max-height: 200px;
+    overflow-y: auto;
+  }
+  
+  .input-container {
+    display: flex;
+  }
+  
+  input {
+    flex: 1;
+    padding: 5px;
+  }
+  
+  button {
+    padding: 5px 10px;
+    margin-left: 5px;
+  }
+  </style>
+  
--- front-end/src/views/Projects.vue
+++ front-end/src/views/Projects.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
-    <div>
-      <h1>Projects</h1>
-      <p>TO DO make components for database</p>
-    </div>
-
+    <h1>Project Page</h1>
+    <p>You can make a new project here.</p>
+    <p>Just type in the bar below the name of the new project,</p>
+    <TerminalComponent />
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import TerminalComponent from '@/components/TerminalComponent.vue';
 
+export default {
+  components: {
+    TerminalComponent,
+  },
+};
 </script>
 
 <style scoped>
+/* Add your page-specific styles here */
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
 
+p {
+  text-align: center;
+  margin-bottom: 10px;
+}
 </style>
