08c32c388cf3f89c8f1b7d0c6e807fb2f85d229c
Thomas van Vliet <113383928+thomas-321@users.noreply.github.com>
2024-01-22 20:09:25 +0100

    Updated java version

--- setup/install/install-java-raspberry-pi.sh
+++ setup/install/install-java-raspberry-pi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the Java package
-curl -o openjdk-21.tar.gz https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.tar.gz
+curl -o openjdk-21.tar.gz https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-aarch64_bin.tar.gz
 
 # Install Java
 sudo mkdir -p /opt/jdk
@@ -9,8 +9,8 @@ sudo tar -zxf openjdk-21.tar.gz -C /opt/jdk
 rm openjdk-21.tar.gz
 
 # Set up alternatives
-sudo update-alternatives --install /usr/bin/java java /opt/jdk/jdk-21/bin/java 21
-sudo update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk-21/bin/javac 21
+sudo update-alternatives --install /usr/bin/java java /opt/jdk/jdk-21.0.2/bin/java 21
+sudo update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk-21.0.2/bin/javac 21
 
 # Check if Java is correctly installed
 java -version
